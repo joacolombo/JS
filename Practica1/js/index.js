ee3156e90ff4c634e412caac83acef512c3a68ce
Joaquin Colombo <joacolombo@outlook.com>
2021-03-17 21:54:45 -0300

    Tipos de datos y variables

--- Practica1/js/index.js
+++ Practica1/js/index.js
@@ -1,8 +1,25 @@
 let paragraph = document.getElementById("mainParagraph");
+let btnNumeric = document.getElementById("numeric");
+let btnText = document.getElementById ("text");
+let btnBool = document.getElementById ("bool");
 
-paragraph.addEventListener("click", WriteParagraph);
+btnNumeric.addEventListener("click", writeNumericValue)
 
-function WriteParagraph(){
-    paragraph.textContent = "Hola Mundo";
+btnText.addEventListener("click", writeTextValue)
+
+btnBool.addEventListener("click", writeBoolValue)
+
+function writeNumericValue(){
+    let numericValue = 2 + 2; //Tipo dato number
+    paragraph.textContent = numericValue;
 }
 
+function writeTextValue(){
+    let textValue = "Hola Mundo";
+    paragraph.textContent = textValue; //Tipo dato string
+}
+
+function writeBoolValue(){
+    let boolValue = true; // 1/0 1=true y 0=false
+    paragraph.textContent = boolValue==1;
+}
