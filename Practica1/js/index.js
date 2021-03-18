90077ed56a4974e2d09f8d7d1ab69a92fc026f63
Joaquin Colombo <joacolombo@outlook.com>
2021-03-17 21:15:46 -0300

    Primer ejemplo con javascript

---
+++ Practica1/js/index.js
@@ -0,0 +1,8 @@
+let paragraph = document.getElementById("mainParagraph");
+
+paragraph.addEventListener("click", WriteParagraph);
+
+function WriteParagraph(){
+    paragraph.textContent = "Hola Mundo";
+}
+
