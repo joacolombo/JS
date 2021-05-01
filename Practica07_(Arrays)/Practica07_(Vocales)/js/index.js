3d355305192c328ede47e575add9b2acb3bded6b
Joaquin Colombo <joacolombo@outlook.com>
2021-05-01 15:43:47 -0300

    Practica06 Arreglo de vocales /Falta validacion de 1 sola palabra/

---
+++ Practica07_(Arrays)/Practica07_(Vocales)/js/index.js
@@ -0,0 +1,122 @@
+const btnApp = document.getElementById("btnApp");
+
+
+btnApp.addEventListener("click", almacenarArray);
+
+let array = [];
+let arrayVocales = [];
+let index = 0;
+
+function almacenarArray(){
+    console.group("ALMACENAR");
+
+    let continuar = true;
+
+    while (continuar == true){
+        let palabra = prompt("Ingrese una palabra");
+
+        //Falta validar que me ingrese una sola palabra
+        
+        if(isNaN(palabra) == false && palabra != null && palabra != ""){
+            alert("Ingrese una palabra");
+        }else if(palabra == null || palabra == ""){
+            continuar = false;
+        }else{
+            array[index] = palabra;
+            console.log(array[index]);
+            index++;
+        }
+    }
+    validarArray();
+}
+
+function validarArray(){
+    console.log("VALIDAR");
+    console.log(array[2].charAt());
+    let index2 = 0;
+
+    for(let index=0; index<array.length; index++){
+        let letra = array[index].charAt();
+
+        if(letra == "a"){
+            arrayVocales[index2] = array[index];
+            index2++;
+        }else if(letra == "e"){
+            arrayVocales[index2] = array[index];
+            index2++;
+        }else if(letra == "i"){
+            arrayVocales[index2] = array[index];
+            index2++;
+        }else if(letra == "o"){
+            arrayVocales[index2] = array[index];
+            index2++;
+        }else if(letra == "u"){
+            arrayVocales[index2] = array[index];
+            index2++;
+        }
+    }
+    mostrarArray();
+}
+
+function mostrarArray(){
+    console.log("MOSTRAR");
+
+    let resultado = "";
+
+    for(let index=0; index<arrayVocales.length; index++){ 
+        resultado += `<li class="list-group-item"> Array[${index}] = ${arrayVocales[index]} </li>`;
+    }
+
+    resultContainer.innerHTML = resultado;
+}
+
+/*
+function random(min, max){
+    console.log("ALMACENO"); 
+    let numeroAleatorioGenerado = Math.floor((Math.random() * (max - min + 1)) + min);
+    console.log(numeroAleatorioGenerado);
+    return numeroAleatorioGenerado;
+}
+
+function determinarLongitud(event){
+    console.log("PASO 1");
+    if(event.key != "Enter"){
+        return;
+    }else if(isNaN(inpLongitud.value) == true){
+        alert('Favor de ingresar un número entero')
+        return;
+    }else if(Number.isInteger(Number(inpLongitud.value)) == false){
+        alert('Favor de ingresar un número entero')
+        return;
+    }else{
+        longitud = Number(inpLongitud.value);
+    }
+
+    inpLongitud.disabled = true;
+    btnMostrarValores.disabled = false;
+    almacenarValores();
+}
+
+function almacenarValores(){ 
+    console.log("PASO 2"); 
+    for(let index=0; index<longitud; index++){
+        array[index] = random(0,9);
+        console.log(array);
+    }
+}
+
+function mostrarValores(){
+    console.log("PASO 3"); 
+    let resultado = "";
+    let suma = 0;
+
+    for(let index=0; index<longitud; index++){
+        suma += array[index];
+        resultado += `<li class="list-group-item"> Array[${index}] = ${array[index]} </li>`;
+    }
+
+    resultado +=`<li class="list-group-item"> La suma de los valores es ${suma} </li>`;
+
+    resultContainer.innerHTML = resultado;
+}
+*/
