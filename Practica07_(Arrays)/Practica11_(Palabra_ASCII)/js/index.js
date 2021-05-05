5974e6a533d201d0fc6c47b6b571317ce932db00
Joaquin Colombo <joacolombo@outlook.com>
2021-05-05 15:40:41 -0300

    Palabra + 3 ASCII

--- Practica07_(Arrays)/Practica11_(Palabra_ASCII)/js/index.js
+++ Practica07_(Arrays)/Practica11_(Palabra_ASCII)/js/index.js
@@ -4,26 +4,45 @@ btnApp.addEventListener("click", almacenarArray);
 
 let palabra = [];
 let palabraASCII = [];
+let resultado = "";
+
 
 function almacenarArray(){
     console.group("ALMACENAR");
 
     let palabra = prompt("Ingrese una palabra");
-        
-        if(isNaN(palabra) == false && palabra != null){
-            alert("Ingrese una palabra");
-        }else if(/\s/.test(palabra)){
-            alert("Ingrese una sola palabra");
-        }else if(palabra == null){
-            return;
-        }else{
-            
+
+    if(isNaN(palabra) == false && palabra != null){
+        alert("Ingrese una palabra");
+    }else if(/\s/.test(palabra)){
+        alert("Ingrese una sola palabra");
+    }else if(palabra == null){
+        return;
+    }else{
+        resultado += `<div class="mt-3">
+        <br>
+        Palabra original: [${palabra}]
+        </div>`;
+
+        for(let index=0; index<palabra.length; index++){
+            let codigo = palabra[index].charCodeAt();
+            let codigo3 = codigo + 3;
+            let letra3 = String.fromCharCode(codigo3);
+            palabraASCII.push(letra3);
         }
-        mostrarArray();
+    }
+    mostrarArray();  
 }
 
 function mostrarArray(){
     console.log("MOSTRAR");
 
-    resultContainer.innerHTML = palabraReves;
+    resultado += `<div class="mt-3">
+    <br>
+    Palabra resulante: [${palabraASCII}]
+    </div>`;
+
+    resultContainer.innerHTML = resultado;
+
+    palabraASCII = [];
 }
